75e14ec8a6b490d02f0cf687d208727acc354162
Connor Ashlock <connor9804@yahoo.com>
2020-08-19 10:55:12 -0700

    App works with one html page now by adding functoins to main.js and addEventListeners on home button and dog button

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
-const advice = $.ajax({
-  url: "https://api.adviceslip.com/advice",
-  success: data => console.log(advice)
-})
+const dogBtn = document.getElementById('dog-btn');
+const homeBtn = document.getElementById('home-btn');
+const dogParent = document.getElementById('dog-row');
+const quoteParent = document.getElementById('advice-row');
 
-const dog = $.ajax({
-  url: "https://random.dog/woof.json",
-  success: data => console.log(dog)
-})
+dogBtn.addEventListener('click', renderDogAdvice);
+homeBtn.addEventListener('click', RemoveDogAdvice)
 
-// function createImg() {
+function renderDogAdvice() {
+  const advice = $.ajax({
+    url: "https://api.adviceslip.com/advice",
+    success: () => {
+      console.log(advice)
+      const response = JSON.parse(advice.responseText);
+      const quote = document.createElement('P');
+      quote.textContent = (response.slip.advice);
+      quote.classList = 'col-11 p-3 quote text-center';
+      quoteParent.appendChild(quote);
+    },
+    error: error => console.log(error)
+  })
 
-// }
+  const dog = $.ajax({
+    url: "https://random.dog/woof.json?filter=mp4,webm",
+    success: () => {
+      console.log(dog)
+      const dogImg = document.createElement('IMG');
+      dogImg.src = dog.responseJSON.url;
+      dogImg.alt = 'Dog';
+      dogImg.classList = 'col-11 p-0 dog';
+      dogParent.appendChild(dogImg);
+    },
+    error: error => console.log(error)
+  })
+  hideDogBtn()
+}
+
+function RemoveDogAdvice() {
+  dogParent.innerHTML = '';
+  quoteParent.innerHTML = '';
+  showDogBtn();
+}
+
+function showDogBtn() {
+  dogBtn.parentElement.classList.remove('d-none');
+}
+
+function hideDogBtn() {
+  dogBtn.parentElement.classList.add('d-none');
+}
